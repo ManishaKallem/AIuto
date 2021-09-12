6ec4a26fc554ffe36163e798603e0e92e4b7d0e7
IgnisDa <ignisda2001@gmail.com>
2021-09-12 10:36:20 +0550

    feat: initial commit

---
+++ .devcontainer/scripts/base.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# setup the git identities
+git config --global user.name "${GIT_AUTHOR_NAME}"
+git config --global user.email "${GIT_AUTHOR_EMAIL}"
+
+# remove the default fish shell prompt
+set fish_greeting
+
+# own the HOME cache directory
+sudo chown -R $USERNAME:$USERNAME $HOME/.cache
+
+# Install the dependencies of each micro-service
+PROJECT_FOLDER="/workspace/AIuto"
+cd "$PROJECT_FOLDER"
+
+for filename in $PROJECT_FOLDER/.devcontainer/scripts/private-*; do
+    [ -e "$filename" ] || continue
+    . "$filename"
+done
