482d97d3cbd669994da90e38aeced351f1dd7f53
IgnisDa <ignisda2001@gmail.com>
2021-09-12 12:50:44 +0550

    feat(seed.ts): add database seeding script

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,14 +1,49 @@
 import { PrismaClient } from '@prisma/client';
+import * as faker from 'faker';
+import { randomUUID } from 'crypto';
 
 const prisma = new PrismaClient();
 
-async function seed() { }
+function randomElement(arr: unknown[]) {
+  if (arr.length === 0) {
+    throw new Error('Cannot select element from empty array');
+  }
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+async function seed() {
+  await prisma.user.upsert({
+    where: {
+      username: 'admin',
+    },
+    update: {},
+    create: {
+      username: 'admin',
+      email: 'admin@email.com',
+      password: 'admin',
+    },
+  });
+  const usersData = [];
+  for (let i = 0; i < 100; i++) {
+    usersData.push({
+      username: `${faker.internet.userName()}__${randomUUID()}`,
+      email: `${randomUUID()}-${faker.internet.email()}`,
+      password: `${faker.internet.password()}__${randomUUID()}`,
+    });
+  }
+  await prisma.user.createMany({
+    data: usersData,
+  });
+  const users = await prisma.user.findMany({
+    select: { id: true },
+  });
+}
 
 seed()
-    .catch((e) => {
-        console.error(e);
-        process.exit(1);
-    })
-    .finally(async () => {
-        await prisma.$disconnect();
-    });
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
