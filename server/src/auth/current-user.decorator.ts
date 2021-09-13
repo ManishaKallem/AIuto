3c060fe0bca45161d5d7ec00d1023c91da653fea
IgnisDa <ignisda2001@gmail.com>
2021-09-13 12:06:21 +0550

    refactor(current-user.decorator.ts, gql-auth.guard.ts): remove graphql

--- server/src/auth/current-user.decorator.ts
+++ server/src/auth/current-user.decorator.ts
@@ -1,12 +1,8 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { GqlExecutionContext } from '@nestjs/graphql';
 
 export const CurrentUser = createParamDecorator(
-  (_data, context: ExecutionContext) => {
-    if (context.getType() === 'http') {
-      return context.switchToHttp().getRequest().user;
-    }
-    const ctx = GqlExecutionContext.create(context);
-    return ctx.getContext().req.user;
+  (_data, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    return request.user;
   },
 );
--- server/src/auth/guards/gql-auth.guard.ts
+++ server/src/auth/guards/gql-auth.guard.ts
@@ -1,22 +1,5 @@
-import {
-  ExecutionContext,
-  ForbiddenException,
-  Injectable,
-} from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { GqlExecutionContext } from '@nestjs/graphql';
 
 @Injectable()
-export class GqlAuthGuard extends AuthGuard('jwt') {
-  getRequest(context: ExecutionContext) {
-    const ctx = GqlExecutionContext.create(context);
-    return ctx.getContext().req;
-  }
-
-  handleRequest(err: any, user: any) {
-    if (err || !user) {
-      throw err || new ForbiddenException('Could not authenticate with token');
-    }
-    return user;
-  }
-}
+export class ApiAuthGuard extends AuthGuard('jwt') {}
