47268e66aad8743bfd2da38f3f699975bed58c6b
IgnisDa <ignisda2001@gmail.com>
2021-09-13 12:06:35 +0550

    feat(auth): add endpoints for authentication

---
+++ server/src/auth/auth.controller.ts
@@ -0,0 +1,35 @@
+import {
+  Body,
+  Controller,
+  Get,
+  UnauthorizedException,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CurrentUser } from './current-user.decorator';
+import { LoginUserInput } from './dto/login-user.input';
+import { ApiAuthGuard } from './guards/gql-auth.guard';
+import { User } from '@prisma/client';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Get('login')
+  async loginUser(@Body() loginUserInput: LoginUserInput) {
+    const res = await this.authService.validateUserByPassword(loginUserInput);
+    if (res) return res;
+    throw new UnauthorizedException('Incorrect credentials provided');
+  }
+
+  @UseGuards(ApiAuthGuard)
+  @Get('refresh-token')
+  async refreshToken(@CurrentUser() user: User) {
+    const maybeToken = this.authService.createJwt(user);
+    if (!maybeToken)
+      throw new UnauthorizedException('Incorrect credentials provided');
+    return {
+      token: maybeToken,
+    };
+  }
+}
