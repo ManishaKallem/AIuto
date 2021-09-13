f546c1156567405c738f795c6d251748e70be5f6
IgnisDa <ignisda2001@gmail.com>
2021-09-13 10:43:59 +0550

    feat(user.controller.ts, user.service.ts): add controllers for user

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,4 +1,53 @@
-import { Controller } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  ConflictException,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateUserInput } from './dto/create-user.input';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async create(@Body() createUserInput: CreateUserInput) {
+    const resp = await this.userService.createUser(createUserInput);
+    if (resp.status) return resp;
+    throw new BadRequestException(resp);
+  }
+
+  @Get()
+  async findAll() {
+    return await this.userService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    const resp = await this.userService.findOne(id);
+    if (!resp) throw new NotFoundException('The given journal was not found');
+    return resp;
+  }
+
+  //   @Patch(':id')
+  //   async update(
+  //     @Param('id') id: string,
+  //     @Body() updateJournalDto: UpdateJournalDto,
+  //   ) {
+  //     return this.userService.update(id, updateJournalDto);
+  //   }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string) {
+    const wasDeleted = await this.userService.remove(id);
+    if (!wasDeleted) throw new ConflictException('Unable to delete this user');
+    return wasDeleted;
+  }
+}
--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserInput } from './dto/create-user.input';
-import { checkPropertiesExists, mergeObjects, validateObject } from 'src/utils';
-import { CreateUserError } from './dto/create-user.result';
-import { plainToClass } from 'class-transformer';
+import { checkPropertiesExists } from 'src/utils';
 import { PrismaService } from 'src/prisma.service';
 import { getPasswordDigest } from './utils';
 
@@ -12,7 +10,7 @@ export class UserService {
     prisma.$use(async (params, next) => {
       if (
         params.model === 'User' &&
-        (params.action === 'update' || params.action === "create")
+        (params.action === 'update' || params.action === 'create')
       ) {
         if (params.args.data.password) {
           params.args.data.password = await getPasswordDigest(
@@ -24,42 +22,71 @@ export class UserService {
     });
   }
 
+  async findAll() {
+    return await this.prisma.user.findMany({
+      select: {
+        id: true,
+        username: true,
+        email: true,
+      },
+    });
+  }
+
+  findOne(id: string) {
+    const maybeUser = this.prisma.user.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    if (!maybeUser) return false;
+    return maybeUser;
+  }
+
   async createUser(createUserInput: CreateUserInput) {
-    let errors = new CreateUserError();
-    const validationErrors = plainToClass(
-      CreateUserError,
-      await validateObject(createUserInput, CreateUserInput),
-    );
+    const errors = {
+      usernameErrors: [],
+      emailErrors: [],
+    };
     const resp = { status: false, resp: null };
     const usernameExists = await this.prisma.user.count({
       where: {
         username: { equals: createUserInput.username },
       },
     });
-    if (usernameExists !== 0) {
+    if (usernameExists !== 0)
       errors.usernameErrors.push('this user already exists');
-    }
     const emailExists = await this.prisma.user.count({
       where: {
         email: { equals: createUserInput.email },
       },
     });
-    if (emailExists !== 0) {
-      errors.emailErrors.push('this email already exists');
-    }
-    // TODO
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'usernameErrors' does not exist on type '{}'.
-    errors = mergeObjects(errors, ...validationErrors);
+    if (emailExists !== 0) errors.emailErrors.push('this email already exists');
     if (!checkPropertiesExists(Object(errors))) {
       resp.resp = errors;
       return resp;
     }
     resp.status = true;
-    resp.resp = await this.prisma.user.create({ data: createUserInput });
+    resp.resp = await this.prisma.user.create({
+      data: createUserInput,
+      select: {
+        id: true,
+        username: true,
+        email: true,
+      },
+    });
     return resp;
   }
 
   async getUserByUsername(username: string) {
     return await this.prisma.user.findUnique({ where: { username } });
+  }
+
+  async remove(id: string) {
+    try {
+      await this.prisma.user.delete({ where: { id } });
+      return true;
+    } catch {
+      return false;
+    }
   }
 }
