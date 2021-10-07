928a3c41942ab5a08a4e9262b96fd5c1c37caf5d
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-10-07 09:54:18 +0550

    style(http.ts): change single to double quotes

--- frontend/src/commons/http.ts
+++ frontend/src/commons/http.ts
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default axios.create({
   baseURL: `${process.env.VUE_APP_API_URL}`,
   headers: {
-    'Content-type': 'application/json',
+    "Content-type": "application/json",
   },
 });
