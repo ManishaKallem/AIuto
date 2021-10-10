c9fe8236808341d8181d88cc5f34b652a5c29f3a
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-10-10 22:03:05 +0550

    feat(Register.vue): add correct form handling

--- frontend/src/views/auth/Register.vue
+++ frontend/src/views/auth/Register.vue
@@ -2,7 +2,7 @@
   <vee-form
     @submit="handleRegister"
     :validation-schema="schema"
-    v-slot="{ isSubmitting }"
+    v-slot="{ isSubmitting, errors }"
   >
     <ion-card>
       <ion-item>
@@ -10,44 +10,77 @@
           <h1>Register</h1>
         </ion-text>
       </ion-item>
+      <ion-item-group>
+        <ion-item>
+          <ion-label>Username</ion-label>
+          <vee-field name="username" v-slot="{ field }">
+            <ion-input
+              type="text"
+              name="username"
+              placeholder="Username"
+              v-bind="field"
+            />
+          </vee-field>
+        </ion-item>
+        <ion-item v-if="errors.username || apiErrors.usernameErrors.length > 0">
+          <ion-text color="danger">
+            <ErrorMessage name="username" as="p" />
+            <p v-for="(error, index) in apiErrors.usernameErrors" :key="index">
+              {{ error }}
+            </p>
+          </ion-text>
+        </ion-item>
+      </ion-item-group>
+      <ion-item-group>
+        <ion-item>
+          <ion-label>Email</ion-label>
+          <vee-field name="email" v-slot="{ field }">
+            <ion-input
+              type="email"
+              name="Email"
+              placeholder="Email"
+              v-bind="field"
+            />
+          </vee-field>
+        </ion-item>
+        <ion-item v-if="errors.email || apiErrors.emailErrors.length > 0">
+          <ion-text color="danger">
+            <ErrorMessage name="email" as="p" />
+            <p v-for="(error, index) in apiErrors.emailErrors" :key="index">
+              {{ error }}
+            </p>
+          </ion-text>
+        </ion-item>
+      </ion-item-group>
+      <ion-item-group>
+        <ion-item>
+          <ion-label>Password</ion-label>
+          <vee-field name="password" v-slot="{ field }">
+            <ion-input
+              type="password"
+              name="password"
+              v-bind="field"
+              placeholder="Password"
+            />
+          </vee-field>
+        </ion-item>
+        <ion-item v-if="errors.password || apiErrors.passwordErrors.length > 0">
+          <ion-text color="danger">
+            <ErrorMessage name="password" as="p" />
+            <p v-for="(error, index) in apiErrors.passwordErrors" :key="index">
+              {{ error }}
+            </p>
+          </ion-text>
+        </ion-item>
+      </ion-item-group>
       <ion-item>
-        <ion-label>Username</ion-label>
-        <vee-field name="username" v-slot="{ field }">
-          <ion-input
-            type="text"
-            name="username"
-            placeholder="Username"
-            v-bind="field"
-          />
-        </vee-field>
-        <ErrorMessage name="username" />
-      </ion-item>
-      <ion-item>
-        <ion-label>Email</ion-label>
-        <vee-field name="email" v-slot="{ field }">
-          <ion-input
-            type="email"
-            name="Email"
-            placeholder="Email"
-            v-bind="field"
-          />
-        </vee-field>
-        <ErrorMessage name="email" />
-      </ion-item>
-      <ion-item>
-        <ion-label>Password</ion-label>
-        <vee-field name="password" v-slot="{ field }">
-          <ion-input
-            type="password"
-            name="password"
-            v-bind="field"
-            placeholder="Password"
-          />
-        </vee-field>
-        <ErrorMessage name="password" />
-      </ion-item>
-      <ion-item>
-        <ion-button type="submit" :disabled="isSubmitting">Submit</ion-button>
+        <ion-button
+          expand="block"
+          type="submit"
+          :disabled="isSubmitting || hasErrors(errors)"
+        >
+          Submit
+        </ion-button>
       </ion-item>
     </ion-card>
   </vee-form>
@@ -56,13 +89,14 @@
 <script lang="ts">
 import userService from '@/services/api/user';
 import {
+  alertController,
   IonButton,
   IonCard,
   IonInput,
+  IonItemGroup,
   IonItem,
   IonLabel,
   IonText,
-  alertController,
 } from '@ionic/vue';
 import { useHead } from '@vueuse/head';
 import { defineComponent } from 'vue';
@@ -77,6 +111,7 @@ export default defineComponent({
   components: {
     IonText,
     IonInput,
+    IonItemGroup,
     IonLabel,
     IonItem,
     IonCard,
@@ -85,7 +120,7 @@ export default defineComponent({
   data: () => ({
     schema: {
       username: 'required|min:3|max:50|alpha_spaces',
-      email: 'required|min:3|max:20|email',
+      email: 'required|min:3|email',
       password: 'required|min:5|max:50',
     },
     apiErrors: {
@@ -118,7 +153,13 @@ export default defineComponent({
           buttons: ['OK'],
         });
         alert.present();
+        this.$router.push({ name: 'auth-login' });
       }
+    },
+    hasErrors(errors: Record<string, string>) {
+      const errorKeys = Object.keys(errors);
+      if (errorKeys.length > 0) return true;
+      return false;
     },
   },
   setup() {
