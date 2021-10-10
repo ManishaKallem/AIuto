e4ce884586d697d7bd50048cb5c302639e5e25de
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-10-10 22:02:57 +0550

    feat(index.ts): name the routes

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,34 +1,30 @@
-import { createRouter, createWebHistory } from "@ionic/vue-router";
-import { RouteRecordRaw } from "vue-router";
+import { createRouter, createWebHistory } from '@ionic/vue-router';
+import { RouteRecordRaw } from 'vue-router';
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: "/",
-    redirect: "/home",
+    path: '/',
+    redirect: '/home',
   },
   {
-    path: "/home",
-    name: "Home",
-    component: () => import("../views/Home.vue"),
+    path: '/home',
+    name: 'home',
+    component: () => import('../views/Home.vue'),
   },
   {
-    path: "/message/:id",
-    component: () => import("../views/ViewMessage.vue"),
-  },
-  {
-    path: "/auth",
-    redirect: "/auth/login",
-    component: () => import("../views/auth/Index.vue"),
+    path: '/auth',
+    redirect: '/auth/login',
+    component: () => import('../views/auth/Index.vue'),
     children: [
       {
-        path: "login",
-        name: "Login",
-        component: () => import("../views/auth/Login.vue"),
+        path: 'login',
+        name: 'auth-login',
+        component: () => import('../views/auth/Login.vue'),
       },
       {
-        path: "register",
-        name: "Register",
-        component: () => import("../views/auth/Register.vue"),
+        path: 'register',
+        name: 'auth-register',
+        component: () => import('../views/auth/Register.vue'),
       },
     ],
   },
