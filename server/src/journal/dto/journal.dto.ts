28482c7285282e54b25880b5eccc36ebfe870fbd
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-10-17 19:14:01 +0550

    feat(journal.dto.ts, update-journal.dto.ts): created common module for journal related dtos

--- server/src/journal/dto/journal.dto.ts
+++ server/src/journal/dto/journal.dto.ts
@@ -1,21 +1,25 @@
-/*
+import { PartialType } from '@nestjs/mapped-types';
+
+/**
 The DTO used to create a journal in the database.
  */
 export class CreateJournalDto {
-  /*
+  /**
   The title of the journal entry
   @example 'Take the dog for a walk'
    */
   title: string;
 
-  /*
+  /**
   A brief description of the journal entry
   @example 'While out on a walk, buy some dog food and a bouncy ball.'
    */
   description?: string;
 
-  /*
+  /**
   If set to true, this journal record would be set as completed.
    */
   completed?: boolean;
 }
+
+export class UpdateJournalDto extends PartialType(CreateJournalDto) {}
--- server/src/journal/dto/update-journal.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateJournalDto } from './create-journal.dto';
-
-export class UpdateJournalDto extends PartialType(CreateJournalDto) {}
