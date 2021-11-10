d64dd4d20fdfa3329e1dc6f4a317f7626d44418b
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-11-10 11:39:01 +0550

    feat(app.module.ts): add prediction module and env vars

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,7 +4,9 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { JournalModule } from './journal/journal.module';
 import { NavigatorModule } from './navigator/navigator.module';
+import { PredictionModule } from './prediction/prediction.module';
 import * as Joi from 'joi';
+import { existsSync } from 'fs';
 
 @Module({
   imports: [
@@ -13,12 +15,20 @@ import * as Joi from 'joi';
       validationSchema: Joi.object({
         JWT_SECRET: Joi.string().required(),
         DATABASE_URL: Joi.string().required(),
+        GOOGLE_APPLICATION_CREDENTIALS: Joi.string()
+          .required()
+          .custom((value) => {
+            if (!existsSync(value))
+              throw new Error(`path '${value}' does not exist`);
+            return value;
+          }),
       }),
     }),
     AuthModule,
     UserModule,
     JournalModule,
     NavigatorModule,
+    PredictionModule,
   ],
   controllers: [],
   providers: [],
