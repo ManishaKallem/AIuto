e45658430350f97a9349d7352aff3edbfe6e3a0b
Garvit Shubham <mailme.garvit@gmail.com>
2021-11-15 09:56:31 +0530

    feat(ACT.vue):basic without image

---
+++ frontend/src/views/ACT.vue
@@ -0,0 +1,54 @@
+<template>
+  <ion-page>
+    <ion-content>
+      <ion-text>
+        <p style="font-size: 250%; text-align: center">
+          Hi! <br />I'm ACT bot
+        </p></ion-text
+      >
+
+      <p style="text-align: center">What category do you want to choose?</p>
+
+      <ion-button fill="solid" expand="block" style="width: 75%; margin: auto">
+        Pschycology <br />
+        predictor
+      </ion-button>
+
+      <ion-button
+        fill="solid"
+        expand="block"
+        style="width: 75%; margin: auto; margin-top: 5%"
+      >
+        Therapist
+      </ion-button>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonButton, IonContent, IonPage, IonText } from '@ionic/vue';
+
+import { defineComponent } from 'vue';
+import { useHead } from '@vueuse/head';
+
+export default defineComponent({
+  components: {
+    IonPage,
+    IonContent,
+
+    IonButton,
+    IonText,
+  },
+  setup() {
+    useHead({
+      title: 'ACT Bot',
+      meta: [
+        {
+          name: 'description',
+          content: 'act bot feature of aiuto',
+        },
+      ],
+    });
+  },
+});
+</script>
