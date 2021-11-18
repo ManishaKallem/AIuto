f56fb142a2da9d13bb3523c8c1c613564870551a
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-11-18 21:08:22 +0550

    feat: move axios interceptor

--- frontend/src/commons/http.ts
+++ frontend/src/commons/http.ts
@@ -1,8 +1,17 @@
-import axios from "axios";
+import { getStorageItem } from '@/services/storage';
+import axios from 'axios';
 
-export default axios.create({
+const instance = axios.create({
   baseURL: `${process.env.VUE_APP_API_URL}`,
   headers: {
-    "Content-type": "application/json",
+    'Content-type': 'application/json',
   },
 });
+
+instance.interceptors.request.use(async (config) => {
+  const token = await getStorageItem('token');
+  config.headers['authorization'] = token ? `Bearer ${token}` : '';
+  return config;
+});
+
+export default instance;
