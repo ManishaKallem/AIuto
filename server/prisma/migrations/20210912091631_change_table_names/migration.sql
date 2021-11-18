07c52d8e54fd8887e71f2baccb598cf7b6341426
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-11-18 21:06:49 +0550

    feat(migrations): reset migrations
    random

--- server/prisma/migrations/20210912091631_change_table_names/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
- Warnings:
- 
- - You are about to drop the column `remindEvery` on the `Schedule` table. All the data in the column will be lost.
- 
- */
--- AlterTable
-ALTER TABLE "Schedule"
-  RENAME COLUMN "remindEvery" TO "remindBefore";
