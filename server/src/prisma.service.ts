9f4aa0966c888d907164ad4a2f78d6d48222ad54
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-11-18 11:58:23 +0550

    refactor(prisma.service.ts): fix linting errors

--- server/src/prisma.service.ts
+++ server/src/prisma.service.ts
@@ -4,24 +4,25 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService
-    extends PrismaClient
-    implements OnModuleInit, OnModuleDestroy {
-    constructor() {
-        super({
-            log: [
-                { emit: 'stdout', level: 'query' },
-                { emit: 'stdout', level: 'info' },
-                { emit: 'stdout', level: 'warn' },
-                { emit: 'stdout', level: 'error' },
-            ],
-        });
-    }
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super({
+      log: [
+        { emit: 'stdout', level: 'query' },
+        { emit: 'stdout', level: 'info' },
+        { emit: 'stdout', level: 'warn' },
+        { emit: 'stdout', level: 'error' },
+      ],
+    });
+  }
 
-    async onModuleInit() {
-        await this.$connect();
-    }
+  async onModuleInit() {
+    await this.$connect();
+  }
 
-    async onModuleDestroy() {
-        await this.$disconnect();
-    }
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
 }
