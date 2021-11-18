29d18d2cdb1331e40e38be563bfd26e8e98aaa22
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-11-18 05:33:39 +0000

    fix: set PORT dynamically

--- server/src/main.ts
+++ server/src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
   app.enableCors();
 
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(8000);
+  await app.listen(process.env.PORT || 8000);
 }
 bootstrap();
