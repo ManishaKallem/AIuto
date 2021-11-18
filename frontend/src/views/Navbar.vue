b56b94b4cc2acc22b87ed8ab91e68e864226f3d5
Garvit Shubham <mailme.garvit@gmail.com>
2021-11-18 20:18:55 +0530

    feat(Navbar.vue):Adding bottom tab navbar

---
+++ frontend/src/views/Navbar.vue
@@ -0,0 +1,57 @@
+<template>
+  <ion-page>
+    <ion-tabs
+      @ionTabsWillChange="beforeTabChange"
+      @ionTabsDidChange="afterTabChange"
+    >
+      <ion-tab-bar slot="bottom">
+        <ion-tab-button tab="schedule" href="/nav/scheduler">
+          <ion-icon :icon="calendar"></ion-icon>
+          <ion-label>Scheduler</ion-label>
+          <ion-badge>6</ion-badge>
+        </ion-tab-button>
+
+        <ion-tab-button tab="speakers" href="/nav/act">
+          <ion-icon :icon="personCircle"></ion-icon>
+          <ion-label>ACT</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button tab="group" href="/nav/group">
+          <ion-label>Groups</ion-label>
+          <ion-badge>6</ion-badge>
+        </ion-tab-button>
+      </ion-tab-bar>
+    </ion-tabs>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import {
+  IonIcon,
+  IonLabel,
+  IonPage,
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+} from '@ionic/vue';
+import { calendar, personCircle } from 'ionicons/icons';
+
+export default defineComponent({
+  components: { IonIcon, IonLabel, IonPage, IonTabBar, IonTabButton, IonTabs },
+  setup() {
+    const beforeTabChange = () => {
+      // do something before tab change
+    };
+    const afterTabChange = () => {
+      // do something after tab change
+    };
+    return {
+      calendar,
+      personCircle,
+      beforeTabChange,
+      afterTabChange,
+    };
+  },
+});
+</script>
