d2ab7274e49f553c468ca71e688e575467a88fca
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-11-19 11:18:19 +0550

    fix(Navbar.vue): add required imports

--- frontend/src/views/Navbar.vue
+++ frontend/src/views/Navbar.vue
@@ -4,6 +4,7 @@
       @ionTabsWillChange="beforeTabChange"
       @ionTabsDidChange="afterTabChange"
     >
+      <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="act" href="/nav/act">
           <ion-icon :icon="personCircle"></ion-icon>
@@ -15,7 +16,7 @@
           <ion-label>Stats</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="journal" href="/nav/journal">
+        <ion-tab-button tab="journal" href="/nav/schedules">
           <ion-icon :icon="calendar"></ion-icon>
           <ion-label>Journal</ion-label>
           <ion-badge>6</ion-badge>
@@ -39,11 +40,22 @@ import {
   IonTabBar,
   IonTabButton,
   IonTabs,
+  IonBadge,
+  IonRouterOutlet,
 } from '@ionic/vue';
 import { calendar, personCircle } from 'ionicons/icons';
 
 export default defineComponent({
-  components: { IonIcon, IonLabel, IonPage, IonTabBar, IonTabButton, IonTabs },
+  components: {
+    IonIcon,
+    IonLabel,
+    IonPage,
+    IonTabBar,
+    IonTabButton,
+    IonTabs,
+    IonBadge,
+    IonRouterOutlet,
+  },
   setup() {
     const beforeTabChange = () => {
       // do something before tab change
